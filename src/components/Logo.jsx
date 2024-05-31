099a3ea4a4ca3e388f9bd99246e60a77a6453428
Vk4011 <gvikramram63@gmail.com>
2024-05-31 20:03:51 +0530

    Updated Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from "../assets/DC.png";
+import { Link } from 'react-router-dom';
 import "../styles/Sidebar.css";
 
 const Logo = ({ darkTheme }) => {
   return (
     <div className={`logo ${darkTheme ? 'logo-dark' : 'logo-light'}`}>
-      <div className="logo-icon">
+      <Link className="logo-icon" to="/">
         <img src={logo} alt="" style={{width:"45px",height:"40px"}} />
-      </div>
+      </Link>
     </div>
   );
 };
